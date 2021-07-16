9a8e47c66c8ff1d9156840a59727067ddfa380b9
Ellen <ellen@onuniverse.com>
2021-07-15 23:44:37 -0400

    pretend pagination

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { QueryClientProvider, QueryClient, useQuery } from "react-query";
+import {
+  QueryClientProvider,
+  QueryClient,
+  useInfiniteQuery,
+} from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import axios from "axios";
 
-const fetchPosts = (_, { page }) =>
+const fetchPosts = (_, page = 1) =>
   axios
     .get(`https://jsonplaceholder.typicode.com/posts`, {
-      // params: {
-      //   pageSize: 10,
-      //   pageOffset: page,
-      // },
+      params: {
+        pageSize: 10,
+        pageOffset: page,
+      },
     })
     .then(res => res.data);
 
@@ -25,31 +29,36 @@ const Posts = () => {
 
   const [page, setPage] = useState(1);
 
-  const postsQuery = useQuery(["posts", { page }], fetchPosts);
+  const postsQuery = useInfiniteQuery("posts", fetchPosts, {
+    getFetchMore: (lastPage, allPages) => lastPage.items.nextPageOffset
+  });
 
   useEffect(() => {
     queryClient.prefetchQuery(
-      ["posts", { page: postsQuery.latestData?.nextPageOffset }],
+      "posts",
       fetchPosts
     );
-  }, [postsQuery]);
+  }, []);
 
   return postsQuery.isLoading ? (
     "Loading.."
-  ) : (
+  ) : postsQuery.data? (
     <div>
       <br />
-      <nr />
+      <br />
       {postsQuery.isLoading || postsQuery.isIdle ? (
         "Loading posts"
       ) : (
-        <div>Post count: {postsQuery.data?.length}</div>
-      )}
-      <button onClick={() => setPage(old => old - 1)}>Previous</button>
-      <span>Current page: {page}</span>
-      <button onClick={() => setPage(old => old - 1)}>Next</button>
+        <ul>
+          <h1>List</h1>
+          {postsQuery.data.pages?.map(page => (
+            page.map(el => <li>{el.title}</li>)
+          ))}
+        </ul>
+      )}  
+      <button onClick={() => postsQuery.fetchNextPage()}>Get more</button>
     </div>
-  );
+  ): "";
 };
 
 export default function App() {
